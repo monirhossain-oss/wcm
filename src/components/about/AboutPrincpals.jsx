447a9b59244fee4e9565285a736959387f610816
shiuly-28 <shulybd1245@gmail.com>
2026-03-03 10:53:39 +0600

    add AboutPresting manifesto section with Next.js image optimization

---
+++ src/components/about/AboutPrincpals.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPrincpals = () => {
+    return (
+        <div>
+            <h1>Princepals</h1>
+        </div>
+    );
+};
+
+export default AboutPrincpals;
